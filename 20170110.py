b89586b0e343920084433f8854bee01e58cf7a53
las1982 <lashkov_alex@mail.ru>
2017-01-10 09:32:09 +0300

    just commit message 3

--- 20170110.py
+++ 20170110.py
@@ -1,1 +1,30 @@
 #! /bin/env/ python
+s = """bdfhd'df'sdh'd'hs'''d''
+sdhdDSH"D"hds'h"dh"dh
+
+h
+dfh"sd"h
+'''"""
+print s
+for l in s:
+    print l
+
+import string
+string.join('.','jljlj')
+"""
+join
+split
+rsplit
+partition
+rpartition
+splitlines
+index
+find
+rindex
+rfind
+count
+startswith
+endswith
+replace
+
+"""
