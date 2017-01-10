d6ddf9bb737af983521337791535e7937425627b
las1982 <lashkov_alex@mail.ru>
2017-01-10 10:42:50 +0300

    just commit message 3

--- 20170110.py
+++ 20170110.py
@@ -48,10 +48,24 @@ istitle
 isupper
 unicode
 """
+# formatting output:
+print "asfs sg1+2 is {0}".format(1,2)
 
+for i in range(1,11):
+    print '{0:2d} {0:3d}'.format(i*i)
 
+print dir(string)
+print string.ascii_uppercase
 
+#import locale
+#locale.setlocale(locale.LC_ALL, 'ru_RU')
 
+"""
+string.capwords(s[,sep])
+string.maketrans(from, to)
+
+"""
+print float(' 1 c')
 
 
 
