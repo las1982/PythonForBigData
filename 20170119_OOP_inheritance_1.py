20f436a5ee06dffd6742f2e490fee2233993dd95
las1982 <lashkov_alex@mail.ru>
2017-01-19 10:32:26 +0300

    -

--- 20170119_OOP_inheritance_1.py
+++ 20170119_OOP_inheritance_1.py
@@ -6,18 +6,18 @@ class P:
 
 class A1(P):
     def __init__(self):
-        super(P).__init__(self)
+        P.__init__(self)
         self.__a1 = 2
 
 class A2(P):
     def __init__(self):
-        super(P).__init__(self)
+        P.__init__(self)
         self.__a2 = 2
 
 class B(A1, A2):
     def __init__(self):
-        super(A1).__init__(self)
-        super(A2).__init__(self)
+        A1.__init__(self)
+        A2.__init__(self)
 
 o = B()
-o.__dict__
+print o.__dict__
---
+++ 20170119_OOP_inheritance_2.py
@@ -0,0 +1,17 @@
+class A1:
+    def __init__(self):
+        self.a = 1
+        self.__b = 1
+
+class A2:
+    def __init__(self):
+        self.a = 2
+        self.__b = 2
+
+class B(A1, A2):
+    def __init__(self):
+        A1.__init__(self)
+        A2.__init__(self)
+
+o = B()
+print o.__dict__
