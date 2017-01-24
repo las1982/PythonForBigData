ce830034b437634d1f0d0185652b3e2b3767ccb5
las1982 <lashkov_alex@mail.ru>
2017-01-24 10:25:38 +0300

    -

--- 20170124_AQ.py
+++ 20170124_AQ.py
@@ -12,14 +12,23 @@ class Question:
             self.no.ask()
 
 
-class Question:
+class Answer:
     def ask(self):
         print self.text
         if int(raw_input('0 or 1')):
-            if not self.yes:
-                raise loose(self, True)
-            self.yes.ask()
+           raise win(self)
         else:
             if not self.no:
                 raise loose(self, False)
             self.no.ask()
+
+
+root = 'ask question'
+try:
+    root.ask()
+except loose as ans:
+    print ans.res.text
+    # ...
+    # ...
+except win as ans:
+    print ans.res.text
