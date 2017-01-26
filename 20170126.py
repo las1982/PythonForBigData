864c76cfa0e009703ac39c0b42a69147dbd0097f
las1982 <lashkov_alex@mail.ru>
2017-01-26 09:56:45 +0300

    -

--- 20170126.py
+++ 20170126.py
@@ -2,9 +2,10 @@ class A:
     def __init__(self):
         self.__a = 1
     def __getattr__(self, name):
-        if name in ('a', 'b'):
+        if name in ('a', 'b', 'c'):
             return  self.__a
 
 o = A()
 print o.a
 print o.b
+print o.c
