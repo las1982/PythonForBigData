1f9fb72b09fd75245f14fdff9e90b89fead295cd
las1982 <lashkov_alex@mail.ru>
2017-01-31 10:09:30 +0300

    -

--- 20170131.py
+++ 20170131.py
@@ -17,8 +17,18 @@ print O.m(2)
 
 
 def f(a, b = 1):
+    # comment
     v = a + b
     print v
 
 print f.__name__
 print f.__defaults__
+print f.__code__.co_names
+print f.__code__.co_varnames
+print f.__code__.co_argcount
+print f.__doc__
+
+import inspect
+
+print inspect.getargspec(f)
+print inspect.getargspec(f).args
