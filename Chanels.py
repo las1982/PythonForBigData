47e1f46454d517d3e9e7ec26ff4ef1fa844d9283
las1982 <lashkov_alex@mail.ru>
2017-02-05 20:42:28 +0300

    -

---
+++ Chanels.py
@@ -0,0 +1,18 @@
+﻿# -*- coding: utf-8 -*-
+
+out = open('ChanelsOUT.txt', 'w')
+
+df = []
+
+with open('ChanelsIN.txt') as f:
+    for ln in f:
+        row = []
+        start = 0
+        end = 0
+        while True:
+            end = ln.find('	', start)
+            if end == -1: break
+            row.append(ln[start:end].decode('utf-8').encode('utf-8'))
+            start = end + 1
+        print row
+out.close()
