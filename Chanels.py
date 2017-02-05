9bbc3b438ac3bf8f4a860609abddb1f7d0a79c8c
las1982 <lashkov_alex@mail.ru>
2017-02-05 22:20:10 +0300

    -

--- Chanels.py
+++ Chanels.py
@@ -1,9 +1,6 @@
 ﻿# -*- coding: utf-8 -*-
 
-out = open('ChanelsOUT.txt', 'w')
-
 df = []
-
 with open('ChanelsIN.txt') as f:
     for ln in f:
         row = []
@@ -12,7 +9,30 @@ with open('ChanelsIN.txt') as f:
         while True:
             end = ln.find('	', start)
             if end == -1: break
-            row.append(ln[start:end].decode('utf-8').encode('utf-8'))
+            row.append(ln[start:end])
             start = end + 1
-        print row
+        df.append(row)
+
+start = i = 1
+while True:
+    if df[i][0] == df[start][0]:
+        df[i].append(df[start][2].replace('Частота вещания: ', ''))
+        df[i].append(df[start+1][2].replace('Поляризация: ', ''))
+        df[i].append(df[start+2][2].replace('Модуляция: ', ''))
+        df[i].append(df[start+3][2].replace('FEC: ', ''))
+        df[i].append(df[start+4][2].replace('Символьная скорость (SR): ', ''))
+        i += 1
+        if i == df.__len__(): break
+    else:
+        start = i
+
+out = open('ChanelsOUT.txt', 'a')
+for i in df:
+    str = ''
+    i.pop(2)
+    for j in i:
+        str = str + j + '\t'
+    str = str + '\n'
+    out.write(str)
+print df.__len__()
 out.close()
