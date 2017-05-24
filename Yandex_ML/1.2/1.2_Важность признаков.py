fd12b14fc13047b41c67a46711232d1cf3ea136f
las1982 <lashkov_alex@mail.ru>
2017-05-24 20:27:50 +0300

    -

--- Yandex_ML/1.2/1.2_Важность признаков.py
+++ Yandex_ML/1.2/1.2_Важность признаков.py
@@ -1,8 +1,16 @@
 import pandas
 import numpy as np
-from numpy._distributor_init import N
 from sklearn.tree import DecisionTreeClassifier
-import scipy
 
 data = pandas.read_csv('../1.1/titanic.csv', index_col='PassengerId')
-# data = data[['Pclass', 'Fare', 'Age', 'Sex']]
+# print data.head()
+data = data[['Survived', 'Pclass', 'Fare', 'Age', 'Sex']].dropna()
+predictor = data[['Pclass', 'Fare', 'Age']]
+predictor['Sex'] = data['Sex'].replace(to_replace=['male', 'female'], value=[0, 1])
+print predictor.head()
+response = data[['Survived']]
+print predictor.__len__(), response.__len__()
+
+clf = DecisionTreeClassifier(random_state=241)
+clf.fit(predictor, response)
+print clf.feature_importances_
