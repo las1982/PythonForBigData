2bae49c93c0702c948f10e456b4571843f48b02a
las1982 <lashkov_alex@mail.ru>
2018-03-13 09:49:01 +0300

    Update 20161227_Filter_Map_Reduce_Lambda.py

--- 20161227_Filter_Map_Reduce_Lambda.py
+++ 20161227_Filter_Map_Reduce_Lambda.py
@@ -1,18 +1,21 @@
+import functools
+import sys
+
 items = [1, 2, 3, 4, 5]
 squared = []
 for x in items:
     squared.append(x**2)
-print squared
-
+print(squared)
 
 def sqr(x):
     return x**2
-print map(sqr, items)
+
+print([x for x in map(sqr, items)])
 
 sqr2 = lambda x: x**2
-print map(sqr2, items)
+print([x for x in map(sqr2, items)])
 
-print map((lambda x: x**2), items)
+print([x for x in map((lambda x: x ** 2), items)])
 
 
 def square(x):
@@ -24,9 +27,16 @@ def cube(x):
 
 funcs = [square, cube]
 for r in range(5):
-    value = map(lambda x: x(r), funcs)
-    print value
+    value = [x for x in map(lambda x: x(r), funcs)]
+    print(value)
+# sys.exit(0)
 
-print filter(lambda x: x < 0, range(-5,5))
-print reduce(lambda x, y: str(x) + '_*_' + str(y), items)
-print reduce(lambda x, y: x+y, items)
+print([x for x in filter(lambda x: x >= 0, range(-5, 5))])
+print([x for x in range(-5, 5) if x >= 0])
+print(functools.reduce(lambda x, y: str(x) + '_*_' + str(y), items))
+print(functools.reduce(lambda x, y: x + y, items))
+
+print(items[:-1], items[1:], items[2:])
+averages = [(x + y + z) / 2.0 for (x, y, z) in zip(items[:-1], items[1:], items[2:])]
+print(averages)
+print(sum(items))
